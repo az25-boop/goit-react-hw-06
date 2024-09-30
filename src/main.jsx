c352133ee06bbd01eea07acc021189f18ec01296
Pavel_Melnik <129269794+az25-boop@users.noreply.github.com>
2024-09-30 14:05:01 +0300

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "modern-normalize";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Provaider> */}
     <App />
-    {/* </Provaider> */}
   </React.StrictMode>
 );
